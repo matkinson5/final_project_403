ec11f3dc65c6a28061906e9bd15d35b2dce42216
Bronson Horne <bronsonhorne@gmail.com>
2022-12-05 13:23:07 -0700

    BOOTSTWAP

--- final_project_403/settings.py
+++ final_project_403/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'is_resources',
         'USER': 'postgres',
-        'PASSWORD' : 'admin',
+        'PASSWORD' : 'Bronson2001',
         'HOST' : 'localhost'
     }
 }
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'ISAAC/static')
+]  
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
