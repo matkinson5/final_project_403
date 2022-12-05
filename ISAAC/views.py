ae4588d44b36f6d63d62f0317a94345a8dd418d6
Joe Lambert <jmlambert58@gmail.com>
2022-12-05 10:45:32 -0700

    create

--- ISAAC/views.py
+++ ISAAC/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import resources
 
 # Create your views here.
 
@@ -6,7 +7,18 @@ def indexPageView(request) :
     return render(request, 'ISAAC/index.html')
 
 def ResourcePageView(request) :
+    if request.method == 'POST':
+
+            resource = resources()
+
+            resource.class_title = request.POST['class_title']
+            resource.resource_name = request.POST['resource']
+            resource.link = request.POST['link']
+
+            resource.save()
+
     return render(request, 'ISAAC/resources.html')
+    
 
 def AboutPageView(request) :
     return render(request, 'ISAAC/about.html')
--- final_project_403/settings.py
+++ final_project_403/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'is_resources',
         'USER': 'postgres',
-        'PASSWORD' : 'poppyField4',
+        'PASSWORD' : 'admin',
         'HOST' : 'localhost'
     }
 }
